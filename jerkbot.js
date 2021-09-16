3c50f22c4d3a73eaa8e6698701403f2e0a3230c6
Jeremy <hercmakesthings@gmail.com>
2021-09-16 17:04:56 -0400

    3rd commit

--- jerkbot.js
+++ jerkbot.js
@@ -5,7 +5,20 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 //const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Our Bot is ready to goooooo');
+    console.log('Our Bot is ready to goooooo!!!');
+});
+
+client.on('message', msg => {
+    if(msg.content === '!hi'){
+        //msg.reply("Hello!");
+        msg.channel.send('Hello!');
+    }
+    else if(msg.content === '!jerkme'){
+        msg.member.roles.add('jerk');
+    }
+    else if(msg.content === '!teachme'){
+        msg.member.roles.add('Student');
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
